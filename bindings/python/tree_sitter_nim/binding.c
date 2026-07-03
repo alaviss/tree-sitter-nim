771ec9418c23f3a75b38ebe68a92914c30907216
alaviss <leorize+oss@disroot.org>
2026-07-02 23:50:09 -0700

    update python scarffolding and add VS 2022 to CI (#111)
    This resolves CI-related issues on Windows and fixes python importing there.

--- bindings/python/tree_sitter_nim/binding.c
+++ bindings/python/tree_sitter_nim/binding.c
@@ -9,8 +9,15 @@ typedef struct TSLanguage TSLanguage;
 TSLanguage *tree_sitter_nim(void);
 
 static PyObject* _binding_language(PyObject *self, PyObject *args) {
-    return PyLong_FromVoidPtr(tree_sitter_nim());
+    return PyCapsule_New(tree_sitter_nim(), "tree_sitter.Language", NULL);
 }
+
+static struct PyModuleDef_Slot slots[] = {
+#ifdef Py_GIL_DISABLED
+    {Py_mod_gil, Py_MOD_GIL_NOT_USED},
+#endif
+    {0, NULL}
+};
 
 static PyMethodDef methods[] = {
     {"language", _binding_language, METH_NOARGS,
@@ -22,10 +29,11 @@ static struct PyModuleDef module = {
     .m_base = PyModuleDef_HEAD_INIT,
     .m_name = "_binding",
     .m_doc = NULL,
-    .m_size = -1,
-    .m_methods = methods
+    .m_size = 0,
+    .m_methods = methods,
+    .m_slots = slots,
 };
 
 PyMODINIT_FUNC PyInit__binding(void) {
-    return PyModule_Create(&module);
+    return PyModuleDef_Init(&module);
 }
